e613197ce4acdcf2f53ddf91428d7add842ec65c
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-10-26 12:54:05 +0500

    final

--- js/animations.js
+++ js/animations.js
@@ -18,31 +18,28 @@ function increaseNumberAnimationStep (i, element, endNumber) {
 
 function initIncreaseNumberAnimation() {
   const element = document.querySelector('.features__clients-count');
-  
   increaseNumberAnimationStep(0, element, 5000);
 }
 
 initIncreaseNumberAnimation();
 
-document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
-    if (event.target.value === 'other') {
-      const formContainer = document.createElement('div');
-      formContainer.classList.add('form__group');
-      formContainer.classList.add('form__other-input'); // Задание 1
-   
-      const input = document.createElement('input');
-      input.placeholder = "Введите ваш вариант";
-      input.type = "text"; // Задание 2
-        
-      formContainer.appendChild(input);
-      document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit')); // Задание 3
-    }
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+  }
+ 
+  // Запуск анимации увеличения числа
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
 
-    const otherInput = document.querySelector('.form__other-input');
-    if (event.target.value !== 'other' && otherInput) { // Задание 5
-    document.querySelector('#form form').removeChild(otherInput); // Задание 4
-    }
-  });
+window.addEventListener('scroll', updateScroll);
 
 function addSmoothScroll(anchor) {
   anchor.addEventListener('click', function (e) {
